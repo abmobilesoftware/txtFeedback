76669a7c7730f6078da0473df42dbad26fb4f3bc
Ando <dragos@txtfeedback.net>
2012-08-24 17:56:14 +0300

    changed strophe to use CORS on IE8/9

--- SmsFeedback_Take4/Scripts/strophe/strophe.js
+++ SmsFeedback_Take4/Scripts/strophe/strophe.js
@@ -2555,7 +2555,9 @@ Strophe.Connection.prototype = {
             // or on a gradually expanding retry window for reconnects
             var sendFunc = function () {
                req.date = new Date();
-               req.xhr.setRequestHeader('Content-Type', 'text/plain');
+               if (!window.XDomainRequest) {
+                  req.xhr.setRequestHeader('Content-Type', 'text/plain');
+               }
                 req.xhr.send(req.data);
                 
             };            
@@ -3602,8 +3604,63 @@ Strophe.Connection.prototype = {
             this._idleTimeout = setTimeout(this._onIdle.bind(this), 100);
         }
     }
+
+
 };
 
+Strophe.addConnectionPlugin("xdomainrequest", {
+   init: function () {
+      if (window.XDomainRequest) {
+         Strophe.debug("using XdomainRequest for IE");
+
+         // override the send method to fire readystate 2
+         if (typeof XDomainRequest.prototype.oldsend == 'undefined') {
+            XDomainRequest.prototype.oldsend = XDomainRequest.prototype.send;
+            XDomainRequest.prototype.send = function () {
+               XDomainRequest.prototype.oldsend.apply(this, arguments);
+               this.readyState = 2;
+               try {
+                  this.onreadystatechange();
+               } catch (e) { }
+            };
+         }
+
+         // replace Strophe.Request._newXHR with the xdomainrequest version
+         Strophe.Request.prototype._newXHR = function () {
+            var fireReadyStateChange = function (xhr, status) {
+               xhr.status = status;
+               xhr.readyState = 4;
+               try {
+                  xhr.onreadystatechange();
+               } catch (e) { }
+            };
+            var xhr = new XDomainRequest();
+
+            xhr.readyState = 0;
+            xhr.onreadystatechange = this.func.bind(null, this);
+            xhr.onload = function () {
+               xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+               xmlDoc.async = "false";
+               xmlDoc.loadXML(xhr.responseText);
+               xhr.responseXML = xmlDoc;
+               fireReadyStateChange(xhr, 200);
+            };
+            xhr.onerror = function () {
+               Strophe.error("Strophe xdr.onerror called");
+               fireReadyStateChange(xhr, 500);
+            };
+            xhr.ontimeout = function () {
+               Strophe.error("Strophe xdr.ontimeout called");
+               fireReadyStateChange(xhr, 500);
+            };
+            return xhr;
+         }
+
+      } else {
+         Strophe.info("XDomainRequest not found. Falling back to native XHR implementation.");
+      }
+   }
+});
 if (callback) {
     callback(Strophe, $build, $msg, $iq, $pres);
 }
