829a6c581836c68ab06a9f26016a9d2364983dcd
Ando <dragos@txtfeedback.net>
2012-09-03 13:18:04 +0300

    Updated support conversation relationship

--- SmsFeedback_Take4/Models/Repositories/EFSmsRepository.cs
+++ SmsFeedback_Take4/Models/Repositories/EFSmsRepository.cs
@@ -158,7 +158,7 @@ namespace SmsFeedback_Take4.Models
           List<SmsMessage> supportConversations = new List<SmsMessage>();
           foreach (var wp in wps)
           {
-              var supportConversation = wp.SupportConversation;
+              var supportConversation = wp.ConversationForSupport;
               var packedSupportConversation = new SmsMessage() { From = supportConversation.From, To = supportConversation.From, Text = supportConversation.Text, TimeReceived = supportConversation.TimeUpdated, Read = supportConversation.Read, ConvID = supportConversation.ConvId, Starred = supportConversation.Starred, ClientDisplayName = supportConversation.Client.DisplayName, ClientIsSupportBot = supportConversation.Client.isSupportClient };
               supportConversations.Add(packedSupportConversation);
           }
