a190b6f24088d6aa99f760287265ceef0cb40409
Ando <dragos@txtfeedback.net>
2012-10-15 15:01:43 +0300

    Adding store routing to mobile website

---
+++ mobile.TxtFeedback_take1/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mobile.TxtFeedback_take1.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult General()
+        {
+           return View();
+        }
+        public ActionResult Error404()
+        {
+           return View();
+        }
+    }
+}
