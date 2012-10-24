cf6225624799439c57ce4216bd9a8113a311d234
Ando <dragos@txtfeedback.net>
2012-10-24 09:14:17 +0300

    Updated dependencies for moderator project

--- Moderator/src/org/helpers/tests/UtilitiesTest.java
+++ Moderator/src/org/helpers/tests/UtilitiesTest.java
@@ -6,12 +6,10 @@ import org.helpers.Utilities;
 import org.junit.Test;
 
 public class UtilitiesTest {
-
 	@Test
 	public void testExtractUserFromAddress() {
 		String address = "mike-pc@txtfeedback.net";
 		String user = "mike-pc";
 		assertEquals(user, Utilities.extractUserFromAddress(address));
 	}
-
 }
