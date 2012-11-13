f2b5b66b4aaa32e57167d0c69e440752b45a0dc5
Mihai Batista <Mihai@Mihai-PC.(none)>
2012-11-13 15:54:52 +0200

    Supportuk REST for demo

---
+++ MvcWebApi/demo.supportuk/Controllers/RulesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Models;
+using System.Web.Mvc;
+
+namespace RestAbmob1.Controllers
+{
+    public class RulesController : ApiController
+    {
+        private const string SHORT_ID = "pagepro";
+        
+        // GET api/rules/abmob1
+        public MsgHandlers Get(string from)
+        {
+            Agent agent1 = new Agent("demo.supportuk@txtfeedback.net", 7);
+            List<Agent> agents = new List<Agent> { agent1 };
+            MsgHandlers listOfAgents = new MsgHandlers(agents);
+            return listOfAgents;
+            throw new HttpResponseException(HttpStatusCode.NotFound);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
