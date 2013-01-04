a77729ed903b45127e2d98c2a67ed135cc146970
Mihai Batista <Mihai@Mihai-PC.(none)>
2013-01-04 16:46:01 +0200

    Measure time spent on REST calls GetHandlers and SaveMessage

--- Moderator/src/org/imrouting/MessageProcessor.java
+++ Moderator/src/org/imrouting/MessageProcessor.java
@@ -1,7 +1,10 @@
 package org.imrouting;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedList;
+import java.util.TimeZone;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -22,18 +25,26 @@ public class MessageProcessor {
 	private RestControllerGateway restGtw;	
 	private LinkedList<Message> failedMessages = new LinkedList<Message>();
 	private static final ScheduledExecutorService worker = Executors.newScheduledThreadPool(5);
+	private static int CALL_DELAY = 2;
 	
 	public MessageProcessor(TxtFeedbackModerator tfm) {
 		moderator = tfm;	
 		restGtw = new RestControllerGateway();
-	}
-	
+	}	
 	public void processInternalPacket(Message message) {
-		System.out.println("Received message = " + message.toXML());
+		//System.out.println("Received message = " + message.toXML());
 		try {
 			TxtPacket internalPacket = new TxtPacket(message.getBody());
+			//DA since the date coming from the Javascript client is unreliable we use the server side time as reference
+			Date now = new Date();
+			internalPacket.setDateSent(now);
 			if (message.getType().equals(Message.Type.ClientMsgDeliveryReceipt)) {
 				// For this message update WasReceivedByClient state in DB 
+				//String ackId = message.getReceivedID();
+				//String[] ackIds = ackId.split("##");
+				// TODO update message with ID = ackIds[1]
+				//String ackDest = message.getAckDestination();
+				//moderator.sendAcknowledgeMessage(ackDest, Message.Type.ClientMsgDeliveryReceipt, ackIds[0]);
 				
 			} else {
 			
@@ -58,13 +69,16 @@ public class MessageProcessor {
 
 	private void SendImMessageToStaff(Message iPacket, TxtPacket internalPacket) {
 		final Message receivedPacket = iPacket;
+		long t1, t2;
 		try {
 			String[] fromTo = internalPacket.getConversationId().split("-");
 			if (!Utilities.extractUserFromAddress(iPacket.getTo().toBareJID()).equals(fromTo[1])) {
+				//this is the case when we have staff site to staff site communication
 				StringBuilder reversedConvIdSb = new StringBuilder();
 				reversedConvIdSb.append(fromTo[1]);
 				reversedConvIdSb.append("-");
 				reversedConvIdSb.append(fromTo[0]);
+				t1 = System.currentTimeMillis();
 				restGtw.saveMessage(
 					internalPacket.getFromAddress(), 
 					internalPacket.getToAddress(), 
@@ -79,7 +93,11 @@ public class MessageProcessor {
 					internalPacket.getBody(), 
 					iPacket.getFrom().toBareJID(),
 					false);
+				t2 = System.currentTimeMillis();
+				System.out.println("\"" + receivedPacket.getID() + "\", \"" + "SAVE\", \"" +  (t2 - t1) + "\"");
 			} else {
+				
+				t1 = System.currentTimeMillis();
 				restGtw.saveMessage(
 						internalPacket.getFromAddress(), 
 						internalPacket.getToAddress(), 
@@ -87,12 +105,19 @@ public class MessageProcessor {
 						internalPacket.getBody(), 
 						iPacket.getFrom().toBareJID(),
 						false);				
+				t2 = System.currentTimeMillis();
+				System.out.println(receivedPacket.getID() + " SAVE: " +  (t2 - t1));
 			}
+			//moderator.sendAcknowledgeMessage(iPacket.getFrom().toBareJID(), Message.Type.ServerMsgDeliveryReceipt, "srvAck", iPacket.getID());
+			
+			t1 = System.currentTimeMillis();			
 			ArrayList<Agent> handlers = restGtw.getHandlersForMessage(Utilities.extractUserFromAddress(iPacket.getTo().toBareJID()), internalPacket.getConversationId(), false);
+			t2 = System.currentTimeMillis();
+			System.out.println("\"" + receivedPacket.getID() + "\", \"GET HANDLERS\", \"" + (t2 - t1) + "\"");
 			for (int i=0; i<handlers.size(); ++i) {
 				String from = internalPacket.getFromAddress();
-				moderator.sendInternalMessage(iPacket.getBody(), from, 
-						handlers.get(i).getUser());			
+				moderator.sendInternalMessage(internalPacket.toXML(), from, 
+						iPacket.getID(), internalPacket.getFromAddress());			
 			}						
 		} catch (RESTException e) {
 			Log.addLogEntry(e.getMessage(), LogEntryType.ERROR, e.getMessage());
@@ -102,7 +127,7 @@ public class MessageProcessor {
 					processInternalPacket(receivedPacket);
 				}				
 			};
-				worker.schedule(task, 5, TimeUnit.SECONDS);
+				worker.schedule(task, CALL_DELAY, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			Log.addLogEntry(e.getMessage(), LogEntryType.ERROR, e.getMessage());			
 		}
@@ -118,21 +143,19 @@ public class MessageProcessor {
 					internalPacket.getBody(),
 					iPacket.getFrom().toBareJID(),
 					false);
-			String internalPackFrom = internalPacket.getFromAddress();
-			String internalPackTo = internalPacket.getToAddress();
-			String internalPackConvId = internalPacket.getConversationId();
-			String rcvPackID = receivedPacket.getID();
-			String rcvPackTo = receivedPacket.getTo().toBareJID();
-			String rcvPackFrom = receivedPacket.getFrom().toBareJID();
+			
+			// The request message sent to the client will have an ID of this format UUID##DBid
+			//String computedId = iPacket.getID() + "##" + "dbID";
 			// Send acknowledge to staff
-			moderator.sendAcknowledgeMessage(rcvPackID, internalPackConvId, rcvPackTo, rcvPackFrom , Message.Type.ServerMsgDeliveryReceipt);
+			//moderator.sendAcknowledgeMessage(iPacket.getFrom().toString(), Message.Type.ServerMsgDeliveryReceipt, "srvAck", iPacket.getID());
 			/* 
 			* send both temporary and db id - the db id will be used on callback 
 			* to mark that the message was successfully received
 			* and the temporary will be forwarded to staff.
-			*/ 
-			moderator.sendInternalMessage(iPacket.getBody(), internalPackFrom,
-					internalPacket.getToAddress());
+			*/
+			
+			/* TODO: replace with computedId. The current ID it's used to trace the message */
+			moderator.sendInternalMessage(iPacket.getBody(), internalPacket.getToAddress(), receivedPacket.getID(), internalPacket.getFromAddress());
 		} catch (RESTException e) {
 				Log.addLogEntry(e.getMessage(), LogEntryType.ERROR, e.getMessage());
 				Runnable task = new Runnable() {
@@ -141,7 +164,7 @@ public class MessageProcessor {
 						processInternalPacket(receivedPacket);
 					}				
 				};
-			worker.schedule(task, 5, TimeUnit.SECONDS);
+			worker.schedule(task, CALL_DELAY, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			Log.addLogEntry(e.getMessage(), LogEntryType.ERROR, e.getMessage());			
 		}
@@ -150,14 +173,19 @@ public class MessageProcessor {
 	
 	private void sendSmsMessageToStaff(Message iPacket, TxtPacket internalPacket) {
 		final Message receivedPacket = iPacket;
-		try {
-			//restGtw.saveMessage(internalPacket.getFromAddress(), internalPacket.getToAddress(), internalPacket.getConversationId(), internalPacket.getBody(), iPacket.getFrom().toBareJID(), true);
-			//String WPTelNumber = getWPForThisAddress(internalPacket.getToAddress());
+		try {				
+			restGtw.saveMessage(
+						internalPacket.getFromAddress(), 
+						internalPacket.getToAddress(), 
+						internalPacket.getConversationId(),
+						internalPacket.getBody(), 
+						iPacket.getFrom().toBareJID(),
+						true);		
 			ArrayList<Agent> handlers = restGtw.getHandlersForMessage(Utilities.extractUserFromAddress(iPacket.getTo().toBareJID()), internalPacket.getConversationId(), true);
 			for (int i=0; i<handlers.size(); ++i) {
 				String from = internalPacket.getFromAddress();
-				moderator.sendInternalMessage(iPacket.getBody(), from, 
-						handlers.get(i).getUser());			
+				moderator.sendInternalMessage(internalPacket.toXML(), from, 
+						handlers.get(i).getUser(), internalPacket.getFromAddress());			
 			}
 		} catch (RESTException e) {
 			Log.addLogEntry(e.getMessage(), LogEntryType.ERROR, e.getMessage());
@@ -167,7 +195,7 @@ public class MessageProcessor {
 					processInternalPacket(receivedPacket);
 				}				
 			};
-			worker.schedule(task, 5, TimeUnit.SECONDS);			
+			worker.schedule(task, CALL_DELAY, TimeUnit.SECONDS);			
 		} catch (Exception e) {
 			Log.addLogEntry(e.getMessage(), LogEntryType.ERROR, e.getMessage());			
 		}
@@ -191,7 +219,7 @@ public class MessageProcessor {
 					processInternalPacket(receivedPacket);
 				}				
 			};
-			worker.schedule(task, 5, TimeUnit.SECONDS);			
+			worker.schedule(task, CALL_DELAY, TimeUnit.SECONDS);			
 		} catch (Exception e) {
 			Log.addLogEntry(e.getMessage(), LogEntryType.ERROR, e.getMessage());			
 		}	
