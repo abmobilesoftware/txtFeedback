9ffd5bcaf4571f18c00eadc0d3f0927dc8fd85b0
Mihai Batista <Mihai@Mihai-PC.(none)>
2013-01-21 14:40:04 +0200

    Message status class

---
+++ SmsFeedback_Take4/Models/Helpers/SubscriptionSmsStatus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmsFeedback_Take4.Models.Helpers
+{
+   public class SubscriptionSmsStatus
+   {
+      public int MessageID { get; set; }
+      public bool MessageSent { get; set; }
+      public bool WarningLimitReached { get; set; }
+      public bool SpendingLimitReached { get; set; }
+      public string WarningLimitReachedMessage { get; set; }
+      public string SpendingLimitReachedMessage { get; set; }
+      
+      public SubscriptionSmsStatus(int msgID, bool msgSent, bool warningLimitReached, bool spendingLimitReached, string warningMSG = "", string spendingMSG= "")
+      {
+         MessageID = msgID;
+         MessageSent = msgSent;
+         WarningLimitReached = warningLimitReached;
+         SpendingLimitReached = spendingLimitReached;
+         WarningLimitReachedMessage = warningMSG;
+         SpendingLimitReachedMessage = spendingMSG;
+      }
+   }
+}
