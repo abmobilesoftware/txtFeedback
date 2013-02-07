6b5eaba931e81a3c06dff188d73f0ff422cde6aa
Ando <dragos@txtfeedback.net>
2013-02-07 17:12:41 +0200

    adding euro gsm account

---
+++ MvcWebApi/eurogsm/Models/MsgHandlers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class MsgHandlers
+    {
+        public List<Agent> agents;
+        
+        public MsgHandlers(List<Agent> iAgents)
+        {
+            agents = iAgents;            
+        }
+    }
+}
