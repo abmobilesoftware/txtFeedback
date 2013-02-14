9673a81f0c3ec2b8a905b42839ad8532a790df54
Mihai Batista <Mihai@Mihai-PC.(none)>
2013-02-14 16:34:28 +0200

    Helper classes for packing the report data in json format

---
+++ SmsFeedback_Take4/Models/Helpers/RepChartDataArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmsFeedback_Take4.Models.Helpers
+{
+    public class RepChartDataArray
+    {
+        public LinkedList<RepChartData> ChartDataArray;
+        public RepChartDataArray(LinkedList<RepChartData> chartDataArray)
+        {
+            ChartDataArray = chartDataArray;
+        }
+    }
+}
---
+++ SmsFeedback_Take4/Models/Helpers/ReportData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmsFeedback_Take4.Models.Helpers
+{
+    public class ReportData
+    {
+        public RepChartDataArray ChartDataArray;
+        public RepInfoBoxArray InfoBoxArray;
+
+        public ReportData(RepChartDataArray chartDataArray, RepInfoBoxArray infoBoxArray)
+        {
+            ChartDataArray = chartDataArray;
+            InfoBoxArray = infoBoxArray;
+        }
+    }
+}
