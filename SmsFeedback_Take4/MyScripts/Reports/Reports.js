12dd0e4f485c44978febfc1aaeff4a79952e2b93
Mihai Batista <Mihai@Mihai-PC.(none)>
2013-03-01 13:19:09 +0200

    Minor refactor

--- SmsFeedback_Take4/MyScripts/Reports/Reports.js
+++ SmsFeedback_Take4/MyScripts/Reports/Reports.js
@@ -51,38 +51,6 @@ var ReportModel = Backbone.Model.extend({
    ]
 });
 
-var Transition = function () {
-   "use strict";
-   var opts = {
-      lines: 13, // The number of lines to draw
-      length: 7, // The length of each line
-      width: 4, // The line thickness
-      radius: 10, // The radius of the inner circle
-      rotate: 0, // The rotation offset
-      color: '#fff', // #rgb or #rrggbb
-      speed: 1, // Rounds per second
-      trail: 60, // Afterglow percentage
-      shadow: true, // Whether to render a shadow
-      hwaccel: false, // Whether to use hardware acceleration
-      className: 'spinner', // The CSS class to assign to the spinner
-      zIndex: 2e9, // The z-index (defaults to 2000000000)
-      top: 'auto', // Top position relative to parent in px
-      left: 'auto' // Left position relative to parent in px
-   };
-   var spinner = new Spinner(opts);
-   var target = document.getElementById('chartArea');
-
-   this.startTransition = function () {
-      spinner.spin(target);
-      $("#overlay").show();
-   };
-
-   this.endTransition = function () {
-      spinner.stop();
-      $("#overlay").hide();
-   };
-};
-
 var ReportsContentArea = Backbone.View.extend({
    el: $("#rightColumn"),
    initialize: function () {
@@ -92,7 +60,7 @@ var ReportsContentArea = Backbone.View.extend({
       this.render();
    },
    render: function () {
-      this.transition = new Transition();
+       this.transition = new Transition($("#chartArea"), $("#overlay"));
       this.transition.startTransition();
 
       var template = _.template($("#report-template").html(), this.model.toJSON());
