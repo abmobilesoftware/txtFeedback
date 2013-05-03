aff3979aa1f967cb6fe342008c2cc0e3713f5d6f
Mihai Batista <mihai@txtfeedback.net>
2013-05-03 19:39:44 +0300

    Detect online/offline, active/background states. Go to messages page in two steps, 1st request the messages and 2nd go to messages page when the list is rendered. Activate transition overlay between these two stages

--- TxtFeedbackMobile/LocalFiles/js/myScripts/App.js
+++ TxtFeedbackMobile/LocalFiles/js/myScripts/App.js
@@ -7,19 +7,27 @@ var AppView = Backbone.View.extend({
 	initialize: function() {
 		_.bindAll(this, "goToConversationsPage",
 				"goToMessagesPage", "back", 
-				"goToLogOnPage", "logOffHandler");
+				"goToLogOnPage", "logOffHandler",
+				"selectConversationHandler",
+				"onlineHandler",
+				"offlineHandler",
+				"pauseHandler",
+				"resumeHandler");
 		$.ajaxSetup({ cache: false });
 		this.dom = {
 				$PAGE_LOGON : $("#logOnPage", this.$el),
 				$PAGE_CONVERSATIONS : $("#conversationsPage", this.$el),
-				$PAGE_MESSAGES: $("#messagesPage", this.$el)
+				$PAGE_MESSAGES: $("#messagesPage", this.$el),
+				$TRANSITION: $("#transition", this.$el)
 		};
 		this.currentState = this.states.LOGON;
 		this.constants = {
 				EVENT_SELECTED_CONVERSATION : "conversationsSelectedEvent",
-				EVENT_MLIST_RENDERED: "messagesListRenderedEvent"
+				EVENT_MLIST_RENDERED: "messagesListRenderedEvent",
+				EVENT_UNAUTHORIZED: "unauthorizedEvent"
 		};
 		this.xmppHandler = new XMPPHandler();
+		// Pages
 		this.logOnModel = new LogOnModel({},
 				{xmppHandler: this.xmppHandler})
 		this.logOnPage = new LogOnPage({model: this.logOnModel, 
@@ -34,24 +42,36 @@ var AppView = Backbone.View.extend({
 					el:this.dom.$PAGE_MESSAGES,
 					xmppHandler: this.xmppHandler
 				});
+		this.transition = new Transition({el: this.dom.$TRANSITION});
+		// Events
 		this.logOnModel.on(this.logOnModel.events.LOGON_SUCCESS, 
 				this.goToConversationsPage);
 		this.logOnModel.on(this.logOnModel.events.LOGOFF_SUCCESS,
 				this.goToLogOnPage);	
 		this.conversationsPage.on("EVENT_LOG_OFF", this.logOffHandler);
+		this.messagesPage.on(this.messagesPage.pageEvents.BACK_EVENT, 
+				this.back);
+		this.messagesPage.on(this.messagesPage.pageEvents.READY,
+				this.goToMessagesPage);		
 		Backbone.on(this.constants.EVENT_SELECTED_CONVERSATION,
-				this.goToMessagesPage);
+				this.selectConversationHandler);
 		Backbone.on(this.constants.EVENT_MLIST_RENDERED,
 				this.goToMessagesPage);
-		document.addEventListener("backbutton", this.back);				
+		Backbone.on(this.constants.EVENT_UNAUTHORIZED,
+				this.goToLogOnPage);
+		document.addEventListener("backbutton", this.back);	
+		document.addEventListener("online", this.onlineHandler);
+		document.addEventListener("offline", this.offlineHandler);
+		document.addEventListener("pause", this.pauseHandler);
+		document.addEventListener("resume", this.resumeHandler);
 	},
 	goToConversationsPage: function() {
 		this.currentState = this.states.CONVERSATIONS;
 		$.mobile.changePage(this.dom.$PAGE_CONVERSATIONS);				
 	},
-	goToMessagesPage: function(conversation) {
-		this.messagesPage.setConversation(conversation);
+	goToMessagesPage: function() {		
 		this.currentState = this.states.MESSAGES;
+		this.transition.end();
 		$.mobile.changePage(this.dom.$PAGE_MESSAGES);
 	},
 	goToLogOnPage: function() {
@@ -70,5 +90,22 @@ var AppView = Backbone.View.extend({
 		this.conversationsPage.logOff();
 		this.messagesPage.logOff();
 		this.xmppHandler.disconnect();
+	}, 
+	selectConversationHandler: function(conversation) {
+		this.transition.start();
+		this.messagesPage.loadConversation(conversation);		
+	},
+	onlineHandler: function() {
+		alert("Application is online");
+	},
+	offlineHandler: function() {
+		alert("No network connection");
+	},
+	pauseHandler: function() {
+		alert("Application is in background");
+	},
+	resumeHandler: function() {
+		alert("Application is active");
 	}
+	
 });
