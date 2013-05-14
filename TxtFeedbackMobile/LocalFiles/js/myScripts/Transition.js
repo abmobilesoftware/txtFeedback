ba12159c900b74eda9c683ed99e8bdfa4b6e2a37
Mihai Batista <mihai@txtfeedback.net>
2013-05-14 18:37:43 +0300

    Transition

--- TxtFeedbackMobile/LocalFiles/js/myScripts/Transition.js
+++ TxtFeedbackMobile/LocalFiles/js/myScripts/Transition.js
@@ -1,10 +1,19 @@
 var Transition = Backbone.View.extend({
+	initialize: function() {
+		/*this.dom = {
+			$LOADING_TEXT: $("span", this.$el)	
+		};*/
+	},
 	start: function() {
-		this.$el.height($(window).height());
-		this.$el.width($(window).width());
+		this.$el.height($(document).height());
+		this.$el.width($(document).width());
+		/*this.dom.$LOADING_TEXT.css("margin-top", 
+				$(window).height() / 2 - 2 * this.dom.$LOADING_TEXT.height());*/
+		//this.$el.fadeIn('fast');
 		this.$el.show();
 	},
 	end: function() {
+		//this.$el.fadeOut('slow');
 		this.$el.hide();
 	}
 });
