1beba01dbc2f45d5ed3b3d6e0a2a105756e1ed24
Mihai Batista <mihai@txtfeedback.net>
2013-06-03 11:31:02 +0300

    Simple structure to hold working point and conversations information

---
+++ SmsFeedback_Take4/Models/Helpers/WpAndConversations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmsFeedback_Take4.Models.Helpers
+{
+   public class WpAndConversations
+   {
+      public WorkingPoint workingPoint;
+      public IEnumerable<SmsMessage> conversations;
+
+      public WpAndConversations(WorkingPoint iWorkingPoint, IEnumerable<SmsMessage> iConversations)
+      {
+         workingPoint = iWorkingPoint;
+         conversations = iConversations;
+      }
+   }
+}
