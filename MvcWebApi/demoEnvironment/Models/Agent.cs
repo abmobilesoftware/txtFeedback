2096ce592795185c02b04a0db5b8464cbe7d99a6
Mihai Batista <mihai@txtfeedback.net>
2013-06-04 17:50:46 +0300

    Blueprint REST service for demo environment

---
+++ MvcWebApi/demoEnvironment/Models/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+   public class Agent
+   {
+      public String xmppId;
+      public List<Device> devices;
+      public int priority;
+
+      public Agent(String iXmppId, List<Device> iDevices, int iPriority)
+      {
+         xmppId = iXmppId;
+         devices = iDevices;
+         priority = iPriority;
+      }
+   }
+}
