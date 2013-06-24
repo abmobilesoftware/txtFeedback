30ad3af54daa163ee8ef41b3ee85520f7ff7d663
Mihai Batista <mihai@txtfeedback.net>
2013-06-24 12:31:39 +0300

    Compute MD5sum

--- Vivacom/Vivacom/Utilities.cs
+++ Vivacom/Vivacom/Utilities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace Vivacom
 {
@@ -29,6 +30,19 @@ namespace Vivacom
             bytes[i/2] = Convert.ToByte(hexInput.Substring(i, 2), 16);
          }
          return UTF8Encoding.UTF8.GetString(bytes);
+      }
+
+      public static string CalculateMD5Hash(string input)
+      {
+         MD5 md5 = MD5.Create();
+         byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+         byte[] hash = md5.ComputeHash(inputBytes);
+         StringBuilder sb = new StringBuilder();
+         for (int i = 0; i < hash.Length; i++)
+         {
+            sb.Append(hash[i].ToString("X2"));
+         }
+         return sb.ToString();
       }
    }
 }
