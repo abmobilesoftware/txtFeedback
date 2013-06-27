c184e11b417f715fb0d0265eabe5046e8a5c10d0
Ando <dragos@txtfeedback.net>
2013-06-27 13:29:31 +0300

    Mark message as unicode encoded if unicode characters are detected

--- SmsFeedback_Take4/Models/Repositories/NexmoSmsRepository.cs
+++ SmsFeedback_Take4/Models/Repositories/NexmoSmsRepository.cs
@@ -45,13 +45,26 @@ namespace SmsFeedback_Take4.Models
          return StaticSendMessage(from, to, message);
       }
 
+      
+      private static bool ContainsUnicodeCharacter(string input)
+      {
+         //This will detect for extended ASCII. If you only detect for the true ASCII character range (up to 127), then you could potentially get false positives for extended ASCII characters which does not denote Unicode
+         //http://blog.platformular.com/2012/03/07/determine-a-string-contains-unicode-character-c/
+         const int MaxAnsiCode = 255;
+         return input.Any(c => c > MaxAnsiCode);
+      }
+
       public static MessageStatus StaticSendMessage(string from, string to, string message)
       {
          var textModel = new TextRequestModel { Text = message };
          var username = "5546d7a3";
-         var password = "48e9a393";
-         
+         var password = "48e9a393";        
          var requestModel = RequestModelBuilder.Create(username, password, from, to, textModel);
+         //find out if we are dealing with unicode characters
+         if (ContainsUnicodeCharacter(message))
+         {
+            requestModel.Type = "unicode";
+         }
          var nexmo = new Nexmo_CSharp_lib.Nexmo();
          JsonResponseModel responseModel = nexmo.Send(requestModel, ResponseObjectType.Json) as JsonResponseModel;
          var msg = responseModel.MessageModels.First();
